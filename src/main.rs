8921ca49f9c063d00465b9bbfc5f140a8370e2e1
soir20 <71418127+soir20@users.noreply.github.com>
2023-08-15 23:44:39 -0400

    Add function to find similar color

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use palette::color_difference::EuclideanDistance;
+use palette::Srgba;
+
+#[derive(Copy, Clone)]
+struct Color {
+    id: u8,
+    srgba: Srgba
+}
+
+impl Default for Color {
+    fn default() -> Self {
+        Color { id: 0, srgba: Srgba::new(0.0, 0.0, 0.0, 0.0) }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
+}
+
+fn find_similar_color(color: Srgba, palette: &Vec<Color>) -> Color {
+    let mut best_distance = f32::MAX;
+    let mut best_color = Color::default();
+
+    for palette_color in palette {
+        let distance = color.distance_squared(*palette_color.srgba);
+
+        if distance < best_distance {
+            best_distance = distance;
+            best_color = *palette_color;
+        }
+    }
+
+    best_color
 }
