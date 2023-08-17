40d119d467fe99b35c77e39427f38c44760816e0
soir20 <71418127+soir20@users.noreply.github.com>
2023-08-17 02:23:37 -0400

    Use u8 colors instead of f32

--- src/main.rs
+++ src/main.rs
@@ -2,42 +2,42 @@ use std::io::Cursor;
 use std::vec::IntoIter;
 use image::{DynamicImage, GenericImageView, ImageResult, Pixel};
 use image::io::Reader;
-use palette::color_difference::{EuclideanDistance, Wcag21RelativeContrast};
+use palette::color_difference::Wcag21RelativeContrast;
 use palette::Srgba;
 
 fn main() {
     println!("Hello, world!");
-    let test: Srgba = Srgba::new(0.5, 0.5, 0.5, 0.5);
-    let test2: Srgba = Srgba::new(0.1, 0.1, 0.1, 0.0);
+    let test = Srgba::<u8>::new(127, 127, 127, 127);
+    let test2 = Srgba::<u8>::new(25, 25, 25, 0);
     println!("{}", distance_squared(test, test2));
 }
 
 #[derive(Copy, Clone)]
 struct Color {
     id: u8,
-    srgba: Srgba
+    srgba: Srgba<u8>
 }
 
 impl Default for Color {
     fn default() -> Self {
-        Color { id: 0, srgba: Srgba::new(0.0, 0.0, 0.0, 0.0) }
+        Color { id: 0, srgba: Srgba::new(0, 0, 0, 0) }
     }
 }
 
 struct ImageColors {
-    colors: Vec<Srgba>,
+    colors: Vec<Srgba<u8>>,
     height: u32
 }
 
 impl ImageColors {
-    fn color(&self, x: u32, y: u32) -> Srgba {
-        self.colors[y * self.height + x]
+    fn color(&self, x: u32, y: u32) -> Srgba<u8> {
+        self.colors[(y * self.height + x) as usize]
     }
 }
 
 impl IntoIterator for ImageColors {
-    type Item = Srgba;
-    type IntoIter = IntoIter<Srgba>;
+    type Item = Srgba<u8>;
+    type IntoIter = IntoIter<Srgba<u8>>;
 
     fn into_iter(self) -> Self::IntoIter {
         self.colors.into_iter()
@@ -52,11 +52,12 @@ impl From<DynamicImage> for ImageColors {
 
         for x in 0..width {
             for y in 0..height {
-                let channels = image.get_pixel(x, y).to_rgba().channels();
-                let red = channels[0] / 255.0;
-                let green = channels[1] / 255.0;
-                let blue = channels[2] / 255.0;
-                let alpha = channels[3] / 255.0;
+                let color = image.get_pixel(x, y).to_rgba();
+                let channels = color.channels();
+                let red = channels[0];
+                let green = channels[1];
+                let blue = channels[2];
+                let alpha = channels[3];
 
                 colors.push(Srgba::new(red, green, blue, alpha));
             }
@@ -77,12 +78,12 @@ fn decode_image_from_bytes(raw_data: &str) -> ImageResult<DynamicImage> {
         .decode()
 }
 
-fn change_palette(original_colors: Vec<Srgba>, palette: &Vec<Color>) -> Vec<Color> {
+fn change_palette(original_colors: Vec<Srgba<u8>>, palette: &Vec<Color>) -> Vec<Color> {
     original_colors.into_iter().map(|color| find_similar_color(color, palette)).collect()
 }
 
-fn find_similar_color(color: Srgba, palette: &Vec<Color>) -> Color {
-    let mut best_distance = f32::MAX;
+fn find_similar_color(color: Srgba<u8>, palette: &Vec<Color>) -> Color {
+    let mut best_distance = u32::MAX;
     let mut best_color = Color::default();
 
     for palette_color in palette {
@@ -97,25 +98,37 @@ fn find_similar_color(color: Srgba, palette: &Vec<Color>) -> Color {
     best_color
 }
 
-fn distance_squared(color1: Srgba, color2: Srgba) -> f32 {
-    let alpha_distance = color1.alpha - color2.alpha;
-    let alpha_distance_squared = alpha_distance * alpha_distance;
-    color1.distance_squared(*color2) + alpha_distance_squared
+fn distance_squared(color1: Srgba<u8>, color2: Srgba<u8>) -> u32 {
+
+    // u8 squared -> u16 needed, u16 x 4 -> u32 needed
+    // Ex: 255^2 * 4 = 260100
+    component_distance_squared(color1.red, color2.red)
+        + component_distance_squared(color1.green, color2.green)
+        + component_distance_squared(color1.blue, color2.blue)
+        + component_distance_squared(color1.alpha, color2.alpha)
+
 }
 
-fn range_relative_luminance(colors: &Vec<Color>) -> Vec<f32> {
-    let min_luma = colors.iter()
-        .map(|color| color.srgba.relative_luminance().luma)
-        .min_by(|a, b| a.partial_cmp(b).expect("Luminance was NaN"))
-        .unwrap_or(0.0);
-    let max_luma = colors.iter()
-        .map(|color| color.srgba.relative_luminance().luma)
-        .max_by(|a, b| a.partial_cmp(b).expect("Luminance was NaN"))
-        .unwrap_or(1.0);
+fn component_distance_squared(component1: u8, component2: u8) -> u32 {
+    let distance = component1.abs_diff(component2) as u32;
+    distance * distance
+}
+
+fn range_relative_luminance(colors: &[Color]) -> Vec<f32> {
+    let (min_luma, max_luma) = colors.iter()
+        .map(|color| {
+            let srgba_f32: Srgba<f32> = color.srgba.into_format();
+            srgba_f32.relative_luminance().luma
+        })
+        .fold((0.0f32, 1.0f32), |(min, max), luma| (min.min(luma), max.max(luma)));
 
     let range = max_luma - min_luma;
 
-    colors.iter().map(|color| color.srgba.relative_luminance().luma)
+    colors.iter()
+        .map(|color| {
+            let srgba_f32: Srgba<f32> = color.srgba.into_format();
+            srgba_f32.relative_luminance().luma
+        })
         .map(|luma| (luma - min_luma) / range)
         .collect()
 }
