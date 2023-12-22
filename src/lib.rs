3fa4712dd1c0eabbedc002748c545dfbd1bbf50c
soir20 <71418127+soir20@users.noreply.github.com>
2023-12-21 21:43:12 -0500

    Remove extra test code

--- src/lib.rs
+++ src/lib.rs
@@ -753,14 +753,10 @@ impl Pixels<RawColor> {
     }
 }
 
-#[cfg(all(test, feature = "default", feature = "image", feature = "ldraw"))]
+#[cfg(all(test, feature = "default"))]
 mod tests {
-    use std::fs::File;
     use std::hash::Hasher;
-    use ::image::io::Reader;
-    use ::image::imageops::FilterType;
     use crate::distance::EuclideanDistancePalette;
-    use crate::ldraw::{LdrawBrick, LdrawColor, write_mosaic};
     use super::*;
 
     #[derive(Clone, Copy, Debug, Eq, PartialEq)]
@@ -1246,22 +1242,5 @@ mod tests {
                 |_, _, _, _| HEIGHT_TWO_UNIT_BRICK
             ).expect_err("should fail with bad height two unit brick error")
         );
-    }
-
-    #[test]
-    fn test() {
-        let palette: Vec<LdrawColor> = vec![ldraw::DARK_RED, ldraw::RED, ldraw::BLACK, ldraw::YELLOW, ldraw::BRIGHT_LIGHT_ORANGE, ldraw::ORANGE, ldraw::BRIGHT_LIGHT_YELLOW];
-
-        let img = Reader::open("maul2.jpg").unwrap().decode().unwrap().resize(64, 64, FilterType::CatmullRom);
-        println!("Image size: {}x{}", img.width(), img.height());
-        let mosaic = Mosaic::from_image(
-            &img,
-            &EuclideanDistancePalette::new(&palette),
-            |l, w, c| 1,
-            |l, w, h, c| LdrawBrick::new_unit(0)
-        ).unwrap();
-
-        let mut buffer = File::create("result3.ldr").unwrap();
-        write_mosaic(&mut buffer, &mosaic, |b| "parts/6141.dat").unwrap();
     }
 }
